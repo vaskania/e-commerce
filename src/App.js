9e8fcbd365681c556d46ae57d5297ba540faf460
vaskania <vnikabadze@unisens.ge>
2022-06-23 12:16:57 +0400

    added sign in form with styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
@@ -10,13 +10,13 @@ const Shop = () => {
 
 const App = () => {
   return (
-      <Routes>
-        <Route path='/' element={<Navigation/>}>
-          <Route index element={<Home/>}/>
-          <Route path='/shop' element={<Shop/>}/>
-          <Route path='/sign-in' element={<SignIn/>}/>
-        </Route>
-      </Routes>
+     <Routes>
+       <Route path='/' element={<Navigation/>}>
+         <Route index element={<Home/>}/>
+         <Route path='/shop' element={<Shop/>}/>
+         <Route path='/auth' element={<Authentication/>}/>
+       </Route>
+     </Routes>
 
   );
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
   // signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from 'firebase/auth'
 import {
   getFirestore,
@@ -61,4 +62,9 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return
   return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+  return await signInWithEmailAndPassword(auth, email, password)
 }
