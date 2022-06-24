9fc0770d5af8bebe6684461dc70e6151d3240f9e
vaskania <vnikabadze@unisens.ge>
2022-06-24 15:07:55 +0400

    added upload data to firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,21 @@
 import { useState, createContext } from "react";
-import PRODUCTS from '../shop-data.json'
+
+// import SHOP_DATA from '../shop-data'
+//
+// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
   products: [],
 })
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS)
+  const [products, setProducts] = useState([])
+
+  // upload SHOP_DATA ONCE to DB
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA)
+  // }, [])
+
   const value = { products }
 
   return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,13 +7,15 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from 'firebase/auth'
 import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  collection,
+  writeBatch
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -39,6 +41,19 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 // export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore()
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey)
+  const batch = writeBatch(db)
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase())
+    batch.set(docRef, object)
+  })
+
+  await batch.commit()
+  console.log('done')
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
   if (!userAuth) return
