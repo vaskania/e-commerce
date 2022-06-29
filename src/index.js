3ea623fa4311e51851fe5940fa70876449a55555
vaskania <vnikabadze@unisens.ge>
2022-06-29 11:15:47 +0400

    added redux store, user reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from './App';
-import { UserProvider } from "./contexts/user.context";
+import { store } from './store/store'
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 
@@ -12,14 +13,14 @@ import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
-     <Router>
-       <UserProvider>
-         <CategoriesProvider>
-           <CartProvider>
-             <App/>
-           </CartProvider>
-         </CategoriesProvider>
-       </UserProvider>
-     </Router>
+     <Provider store={store}>
+       <Router>
+           <CategoriesProvider>
+             <CartProvider>
+               <App/>
+             </CartProvider>
+           </CategoriesProvider>
+       </Router>
+     </Provider>
    </React.StrictMode>
 );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,19 @@
 import { Fragment, useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import CartIcon from "../../components/cart-icon/cart-icon.components";
 import CardDropdown from "../../components/cart-dropdown/card-dropdown.component";
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import { UserContext } from "../../contexts/user.context";
-
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles'
 import { CartContext } from "../../contexts/cart.context";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext)
+  const currentUser = useSelector(selectCurrentUser)
   const { isCartOpen } = useContext(CartContext)
 
   return (
